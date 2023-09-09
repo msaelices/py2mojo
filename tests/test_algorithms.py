5efdf0d7a72723753b881f4a20c3abbce721c51a
Manuel Saelices <msaelices@gmail.com>
2023-09-10 01:56:56 +0200

    New test file for checking complete algorithms

---
+++ tests/test_algorithms.py
@@ -0,0 +1,26 @@
+from helpers import validate
+
+
+def test_fibonacci():
+    validate(
+'''
+def fib(n: int) -> int:
+    """
+    Calculate the nth Fibonacci number.
+    """
+    if n <= 1:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+''',
+'''
+def fib(n: Int) -> Int:
+    """
+    Calculate the nth Fibonacci number.
+    """
+    if n <= 1:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+''',
+    )
