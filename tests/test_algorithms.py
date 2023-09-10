d1f5af3d9acb66e0e443550ffb2fc018d9e724d4
Manuel Saelices <msaelices@gmail.com>
2023-09-10 12:04:03 +0200

    Fix indentation to make the tests pass

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -3,47 +3,47 @@ from helpers import validate
 
 def test_fibonacci():
     validate(
-        '''
-        def fib(n: int) -> int:
-            """
-            Calculate the nth Fibonacci number.
-            """
-            if n <= 1:
-                return n
-            else:
-                return fib(n - 1) + fib(n - 2)
-        ''',
-        '''
-        def fib(n: Int) -> Int:
-            """
-            Calculate the nth Fibonacci number.
-            """
-            if n <= 1:
-                return n
-            else:
-                return fib(n - 1) + fib(n - 2)
-        ''',
+'''
+def fib(n: int) -> int:
+    """
+    Calculate the nth Fibonacci number.
+    """
+    if n <= 1:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+''',
+'''
+def fib(n: Int) -> Int:
+    """
+    Calculate the nth Fibonacci number.
+    """
+    if n <= 1:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+''',
     )
     validate(
-        '''
-        def fib(n: int) -> int:
-            """
-            Calculate the nth Fibonacci number.
-            """
-            if n <= 1:
-                return n
-            else:
-                return fib(n - 1) + fib(n - 2)
-        ''',
-        '''
-        fn fib(n: Int) -> Int:
-            """
-            Calculate the nth Fibonacci number.
-            """
-            if n <= 1:
-                return n
-            else:
-                return fib(n - 1) + fib(n - 2)
-        ''',
+'''
+def fib(n: int) -> int:
+    """
+    Calculate the nth Fibonacci number.
+    """
+    if n <= 1:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+''',
+'''
+fn fib(n: Int) -> Int:
+    """
+    Calculate the nth Fibonacci number.
+    """
+    if n <= 1:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+''',
         level=1,
     )
